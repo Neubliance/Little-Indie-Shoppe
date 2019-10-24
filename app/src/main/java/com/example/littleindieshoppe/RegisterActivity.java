2809d99c87075b205f2dcf83f45323b2293d4027
Neubliance <ds.kilgore02@gmail.com>
2019-10-24 06:07:44 -0400

    initial commit

---
+++ app/src/main/java/com/example/littleindieshoppe/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.littleindieshoppe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText inputName, inputPhoneNumber, inputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        createAccountButton = (Button) findViewById(R.id.register_btn);
+        inputName = (EditText) findViewById(R.id.register_username_input);
+        inputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
+        inputPassword = (EditText) findViewById(R.id.register_password_input);
+    }
+}
